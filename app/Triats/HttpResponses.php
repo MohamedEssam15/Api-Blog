d10b59ced92d8b54d0b608cde25fb485b3c44218
Mohamed Essam <mesam5081@gmail.com>
2023-05-07 20:38:36 +0300

    add the responses triat

---
+++ app/Triats/HttpResponses.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\Triats;
+
+trait HttpResponses {
+    protected function Success($data,$message=null,$code=200){
+    return response()->json([
+        'status'=>'Request was succesfull',
+        'message'=>$message,
+        'data'=>$data
+    ],$code);
+    }
+    protected function Error($data,$message=null,$code){
+        return response()->json([
+            'status'=>'Error has occurre',
+            'message'=>$message,
+            'data'=>$data
+        ],$code);
+        }
+}
